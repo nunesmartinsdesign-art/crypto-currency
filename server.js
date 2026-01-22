857cd4de286245907eabe7aa9000c4719d93e740
nunesmartinsdesign-art <nunesmartinsdesign@gmail.com>
2026-01-22 19:32:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 import express from "express";
+import { gerarFeedXML } from "./update-feed.js";
+
 const app = express();
 
+let feedXML = "";
+
+// gerar feed no arranque
+async function atualizar() {
+  feedXML = await gerarFeedXML();
+  console.log("Feed atualizado");
+}
+
+atualizar();
+setInterval(atualizar, 10000); // 60s
+
+// servir o feed diretamente
+app.get("/feed.xml", (req, res) => {
+  res.set("Content-Type", "application/xml");
+  res.send(feedXML);
+});
+
+// servir o site
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log("Servidor a correr em http://localhost:3000");
-});
+app.listen(3000, () => console.log("Servidor ativo"));
+
